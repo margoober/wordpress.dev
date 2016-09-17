6ff70a7563d856ae889b87c651e2bec5b1051504
Margot McMahon <amcmahon@nes.ru>
2016-09-17 18:12:15 -0400

    Got local test version of JSON to foreach through on the wordpress background. IT'S A START

--- wp-content/plugins/margot-json-converter/margot-json-converter.php
+++ wp-content/plugins/margot-json-converter/margot-json-converter.php
@@ -11,13 +11,13 @@
 
 
 
-   $posts = file_get_contents('local-json-file.php');
+   $posts = file_get_contents('/vagrant/sites/wordpress.dev/public/wp-content/plugins/margot-json-converter/local-json-file.php');
+   //NOTE: Why did I have to include the entire path name?
    $posts = json_decode($posts, true);
 
    foreach ($posts as $post) {
    	print_r($post['title'] . PHP_EOL . $post['content']);
    }
-
 
    // print_r($posts[0]["title"]);
 
